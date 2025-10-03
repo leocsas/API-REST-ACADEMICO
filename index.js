306f2d9e594c19b64d9736b75064857ae60e0cb3
leocsas <leocristian2014@gmail.com>
2025-10-02 22:19:58 -0300

    Adicionando arquivos POSTMAN

--- index.js
+++ index.js
@@ -11,19 +11,21 @@ const alunosRouter = require("./routes/alunos");
 app.use(alunosRouter);
 
 // Membro Cursos - Importar e mapear rota
-// Membro Funcionarios - Importar e mapear rota
-
-// Membro Notas - Importar e mapear rota
 const cursosRouter = require("./routes/cursos");
 app.use(cursosRouter);
+
+// Membro Notas - Importar e mapear rota
+const notasRouter = require("./routes/notas");
+app.use(notasRouter);
 
 // Membro Professores - Importar e mapear rota
 const professoresRouter = require("./routes/professores");
 app.use(professoresRouter);
 
-// Membro Professores - Importar e mapear rota
+// Membro Funcionários - Importar e mapear rota
 const funcionariosRouter = require("./routes/funcionarios");
 app.use(funcionariosRouter);
+
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
